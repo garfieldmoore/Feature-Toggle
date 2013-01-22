34f9a3b9170714ad973d9d26b5cb122dd293f6e6
garfield.moore <garfield.moore@1e.com>
2013-01-22 12:22:07 +0000

    xml comments

--- Toggler/Features.cs
+++ Toggler/Features.cs
@@ -5,13 +5,19 @@ namespace Toggles.Configuration
 {
     public class Features
     {
-        private static ISwitchFactory _switchFactory;
+        private static ISwitchFactory _switchFactory = new FeatureConfigSectionSwitchFactory();
 
         public static bool IsAvailable(string featureName)
         {
             return  _switchFactory.Create().IsAvaliable(featureName);
         }
 
+        /// <summary>
+        /// Sets the factory to use to create the concrete implementation of ISwitch
+        /// </summary>
+        /// <param name="factory">SwitchFactory to use to create concrete implementations of ISwitch</param>
+        /// <remarks>This defaults to the custom Configuration file section</remarks>
+        /// <exception cref="ArgumentNullException">Passing null will throw an exception</exception>
         public static void Initialize(ISwitchFactory factory)
         {
             if (factory == null) throw new ArgumentNullException("factory");
